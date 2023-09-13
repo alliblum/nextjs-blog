491c061ecb4f4ccab3043d5529c50afbd6e84fd1
Allison Blum <118923616+alliblum@users.noreply.github.com>
2023-09-12 23:39:40 -0400

    adding typescript and chakra

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,12 +1,15 @@
 import Link from 'next/link';
+import { Box } from '@chakra-ui/react';
 
 export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
+      <Box bg="gray.100">
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+      </Box>
     </>
   );
 }
