f7148a2b77b57a2e35ad7eeeedc12a55dbf3eb1b
Allison Blum <118923616+alliblum@users.noreply.github.com>
2024-01-03 12:45:57 -0500

    show post data to post page

--- components/post.tsx
+++ components/post.tsx
@@ -1,23 +1,16 @@
-// import { React } from 'react';
-// import { GetStaticProps } from 'next'
-import Layout from './layout'
-// import { getAllPostIds, getPostData } from './posts'
-import Head from 'next/head'
-// import Date from '../../components/date'
-// import utilStyles from '../../styles/utils.module.css';
-import { GetStaticProps, GetStaticPaths } from 'next'
-import { Heading, Box, Stack, Text } from '@chakra-ui/react'
-// import { useMDXComponents } from '../../mdx-components'
-// import { useRouter } from 'next/router'
-// import MDXComponents from '../../components/mdxcomponents'
-import { MDXProvider } from '@mdx-js/react'
-import QuoteBlock from './mdx/quoteblock' // Adjust the path accordingly
-
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import { serialize } from 'next-mdx-remote/serialize'
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
+import { MDXProvider } from '@mdx-js/react'
+import Head from 'next/head'
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { Heading, Box, Stack, Text } from '@chakra-ui/react'
+
+import Layout from './layout'
+import QuoteBlock from './mdx/quoteblock' // Adjust the path accordingly
+import PostData from '../types/PostData'
 
 const MDXComponents = {
   QuoteBlock,
@@ -27,66 +20,25 @@ const Post = ({
   postData,
   source,
 }: {
-  postData: {
-    title: string
-    // date: string
-    contentHtml?: string
-    article?: React.ReactNode
-  }
+  postData: PostData
   source: MDXRemoteSerializeResult
 }) => {
   return (
     <Box>
       <Head>{postData?.title}</Head>
       <Layout>
-        {/* <article>
-          <h1> */}
         <Heading>{postData?.title}</Heading>
-        {/* </h1> */}
+        <p>{postData?.date}</p>
+        <p>{postData?.color}</p>
         {/* Render MDX content */}
-        {postData?.article && (
-          <MDXProvider components={MDXComponents}>
-            {postData?.article}
-          </MDXProvider>
-        )}
-
         <MDXProvider components={MDXComponents}>
           <div className="markdown-body">
             <MDXRemote {...source} components={MDXComponents} />
           </div>
         </MDXProvider>
-
-        {/* <MDXProvider components={MDXComponents}>{postData.article}</MDXProvider> */}
-        {/* You can still render other HTML content if needed */}
-
-        <div className="markdown-body">
-          {/* <Box { postData?.contentHtml } /> */}
-          {/* <Box dangerouslySetInnerHTML={{ __html: postData?.contentHtml }} /> */}
-        </div>
-
-        {/* <text>{postData.contentHtml}</text> */}
-        {/* <text>{postData.contentHtml}</text> */}
-        {/* </article> */}
       </Layout>
     </Box>
   )
 }
-
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   const paths = getAllPostIds()
-//   return {
-//     paths,
-//     fallback: false,
-//   }
-// }
-
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   const postData = await getPostData(params?.id as string)
-//   return {
-//     props: {
-//       postData,
-//     },
-//   }
-// }
 
 export default Post
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,31 +1,25 @@
 // import { React } from 'react';
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
-import Head from 'next/head'
+import { MDXProvider } from '@mdx-js/react'
+import { Heading, Box, Stack, Text } from '@chakra-ui/react'
 import fs from 'fs'
 import { GetStaticProps, GetStaticPaths } from 'next'
-import { Heading, Box, Stack, Text } from '@chakra-ui/react'
-import { useMDXComponents } from '../../mdx-components'
-
-import { MDXProvider } from '@mdx-js/react'
-import Post from '../../components/post'
-
 import path from 'path'
 import matter from 'gray-matter'
 import { serialize } from 'next-mdx-remote/serialize'
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
-// import QuoteBlock from '../../components/mdx/quoteblock' // Adjust the path accordingly
+import Head from 'next/head'
+
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import { useMDXComponents } from '../../mdx-components'
+import Post from '../../components/post'
+import PostData from '../../types/PostData'
 
 const PostPage = ({
   postData,
   source,
 }: {
-  postData: {
-    title: string
-    // date: string
-    contentHtml: string
-    article: React.ReactNode
-  }
+  postData: PostData
   source: MDXRemoteSerializeResult
 }) => {
   return <Post postData={postData} source={source} />
@@ -39,17 +33,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }
 }
 
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   const postData = await getPostData(params?.id as string)
-//   return {
-//     props: {
-//       postData: {
-//         ...postData,
-//         article: postData.article || null,
-//       },
-//     },
-//   }
-// }
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const id = params?.id
   const postsDirectory = path.join(process.cwd(), 'posts')
@@ -57,11 +40,11 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const mdxFilePath = path.join(postsDirectory, `${id}.mdx`) //path.join(process.cwd(), 'content', 'example.mdx')
   const source = fs.readFileSync(mdxFilePath, 'utf8')
   const { content, data } = matter(source)
-
   const mdxSource = await serialize(content)
   return {
     props: {
       source: mdxSource,
+      postData: data,
     },
   }
 }
