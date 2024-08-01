4a2b12920f887fd51df38e1cde7021b0815ac839
Allison Blum <118923616+alliblum@users.noreply.github.com>
2024-08-01 12:50:36 -0400

    adding copy to lp page

---
+++ components/mdx/email-form.tsx
@@ -0,0 +1,7 @@
+export default function EmailForm() {
+  ;<iframe
+    scrolling="no"
+    style="width:100%!important;height:220px;border:1px #ccc solid !important"
+    src="https://buttondown.email/alli?as_embed=true"
+  ></iframe>
+}
--- pages/research-buy-in.tsx
+++ pages/research-buy-in.tsx
@@ -1,5 +1,9 @@
 import { QuoteBlock } from 'components/mdx'
 import Layout from 'components/layout'
+// import ReactHtmlParser from 'react-html-parser'
+
+// import CustomHTMLComponent from './CustomHTMLComponent'
+// import { defineCustomElements } from 'react-custom-element'
 
 import {
   Box,
@@ -12,14 +16,241 @@ import {
   ListItem,
   Center,
   List,
+  OrderedList,
 } from '@chakra-ui/react'
+
+// const CustomHTMLComponent = () => {
+//   const htmlContent = (
+//     <div>
+//       <form
+//         action="https://buttondown.email/api/emails/embed-subscribe/alli"
+//         method="post"
+//         target="popupwindow"
+//         onSubmit="window.open('https://buttondown.email/alli', 'popupwindow')"
+//         class="embeddable-buttondown-form"
+//       >
+//         <label for="bd-email">Enter your email</label>
+//         <input type="email" name="email" id="bd-email" />
+
+//         <input type="submit" value="Subscribe" />
+//         <p>
+//           <a href="https://buttondown.email/refer/alli" target="_blank">
+//             Powered by Buttondown.
+//           </a>
+//         </p>
+//       </form>
+//     </div>
+//   )
+//   return <div>{ReactHtmlParser(htmlContent)}</div>
+// }
+// ;<Box></Box>
 
 export default function EmailDripBuyInMindset() {
   return (
     <Layout home>
       <Stack spacing={5}>
-        <Heading size="xl">Workshop: Is JTBD right for your team?</Heading>
-        <Text></Text>
+        <Heading size="xl">
+          You might think the only way to get buy-in on Jobs To Be Done is to
+          quit your job/fire your clients and find a team that “gets it”
+        </Heading>
+        <Text>
+          But if you believe that your colleagues are (for the most part) smart
+          people.
+        </Text>
+        <Text>
+          And if you believe that (for the most part) your product has the
+          potential to help people.
+        </Text>
+        <Text>
+          And if you believe (for the most part) that your marketing, done
+          right, could get your company meaningful outcomes.
+        </Text>
+        <Text>
+          You don’t have to leave your company. You just need to try change
+          management.{' '}
+        </Text>
+        <Text>Here's how I know:</Text>
+        <Text>
+          A few years ago I ran a JTBD study, identified the job stories, &
+          shared the output with the CEO who hired me.
+        </Text>
+        <Text>
+          "I love this! Present it in all hands." I did, and everyone there
+          seemed to "love" it, too. Emojis in the Zoom chat, DMs after. The
+          works.
+        </Text>
+        <Text>
+          And then... nothing happened. After that project presentation, few
+          people consulted the recordings, the transcripts, or the artifacts.
+          The research didn't make its way into other decision-making centers
+          outside of the ones I was already in.
+        </Text>
+        <Heading size="lg">
+          It's hard enough to get buy-in on doing user research. Or doing Jobs
+          To Be Done research. Or researching your ICP.
+        </Heading>
+        <Heading size="lg">
+          {' '}
+          It's even harder to get buy-in on applying your findings.
+        </Heading>
+        <Text>
+          I was a product marketer at the time. My charter was to improve
+          onboarding and activation metrics and drive adoption of new features.
+          My data was telling me that the product team was shipping features no
+          one wanted while ignoring Grand Canyon sized growth opportunities. But
+          because the JTBD research findings didn’t take hold in the product
+          department. After my presentation, I still found myself pushing a
+          boulder up a hill pitching product features no one wanted.
+        </Text>
+        <Text>
+          <Text>
+            The soul-crushing blow of doing all that work, getting all that
+            data, only for nothing to change.
+          </Text>
+          [meme]
+          <Text>At first I grumbled. Ooooooh how I grumbled.</Text>
+          <Text>"No one is listening to me."</Text>
+          "These ding dongs just don't get it."
+        </Text>
+        <Text>
+          "Man, maybe this research isn't as important as I thought, maybe I got
+          bad advice."
+        </Text>
+        <Text>
+          I didn’t know what to do differently, so I ended up leaving the
+          company.
+        </Text>
+        <Text>
+          I joined a new company and, long story short, the same thing happened.{' '}
+        </Text>
+        <Text>
+          I started to think seriously about the role I was playing in this
+          frustrating outcome.
+        </Text>
+        <Text>
+          I knew that other people were good at mobilizing VOC around their
+          organizations.
+        </Text>
+
+        <Text>
+          I knew that other people were able to ship copy that used raw VOC,
+          build onboarding around customer Jobs To Be Done, ship features for a
+          single ICP.
+        </Text>
+
+        <Text>
+          I knew that other people were turning their ability to mobilize their
+          understanding of a customer into high-paying career paths.
+        </Text>
+
+        <Text>So I had to ask myself: what’s more likely?</Text>
+
+        <Text>
+          Is it more likely that every person I work with is a doofus who
+          doesn’t get it and everyone other teamspeople arework with is
+          naturally good at using VOC?
+        </Text>
+
+        <Text>
+          Or is it more likely that most teams have some people who understand
+          qualitative data and other people who don’t understand its
+          valuequalitative data?{' '}
+        </Text>
+
+        <Text>Is it more likely that everyone around me is a dumb dumb? </Text>
+
+        <Text>
+          Or is it more likely that I am not approaching buy-in for customer
+          research in a way that gets me the alignment, rigor, and brilliant
+          strategic approach that I know my team is capable of.
+        </Text>
+
+        <Text>Until one day I couldn’t ignore it:</Text>
+
+        <Text>[gif or something] It’s me… Hi…. I’m the problem it’s me.</Text>
+        <Heading size="lg">
+          The only common variable in my failed attempts to get buy-in on
+          research was me.
+        </Heading>
+        <Text>
+          Well, me and the 50 or so product, UX, and research people trying to
+          build a more customer centric culture at their companies.
+        </Text>
+
+        <Text>
+          I realized that everything about my approach (and everyone trying to
+          get buy-in) was wrong.
+        </Text>
+
+        <Text>
+          And then wWhen I finally realized the right way to go about getting
+          buy-in, everything changed.
+        </Text>
+
+        <Text>
+          I figured out how to get product and marketing to stop silently
+          seething over who had the most “correct” understanding of their ICP.
+        </Text>
+
+        <Text>
+          I figured out how to get “out of touch” execs to understand why we
+          should or shouldn’t pursue one strategy or another.
+        </Text>
+
+        <Text>
+          I figured out how to get “hardened engineers” to spend an hour reading
+          VOC every day so I’m not the only one pushing back on product features
+          no one wants.
+        </Text>
+
+        <Text>
+          And the first step to making it all happen: breaking down my invisible
+          beliefs and faulty understandings of the work I was actually doing.
+        </Text>
+        <Heading>
+          Your invisible beliefs are holding you back…but they don’t have to any
+          longer{' '}
+        </Heading>
+        <Heading size="lg">
+          Could your invisible beliefs or faulty understanding of what it means
+          to get buy-in on customer research be the thing holding you back from
+          hitting your growth KPIs?
+        </Heading>
+        <Text>
+          I’ve created a free email course to help[some kind of transition here]
+          anyone in B2B SaaS who understands the value of VOC, but struggles to
+          get buy-in, finally get your (and your customers) voices heard.
+        </Text>
+
+        <Text>
+          I’ll send you an email a day outlining the invisible beliefs silently
+          holding you back (without you even realizing it) that hold you back
+          from getting other people to care about and use customer research
+          buy-in on incorporating customer-centric strategies at work - plus the
+          tactics I use to thwart this belief before it gets in my way.
+        </Text>
+        <OrderedList>
+          {' '}
+          <ListItem>
+            Not realizing that introducing JTBD/VOC/CLG is a change management
+            project
+          </ListItem>
+          <ListItem>
+            Not knowing what you mean by “buy-in” or what you want from “change”
+          </ListItem>
+          <ListItem>
+            Not understanding that your colleagues might not make decisions the
+            same way you do
+          </ListItem>
+          <ListItem>
+            Thinking you have to betray your values to get good at
+            organizational politics
+          </ListItem>
+          <ListItem>
+            Thinking that buy-in is about what you get instead of what you give
+          </ListItem>
+        </OrderedList>
+        <Box></Box>
       </Stack>
     </Layout>
   )
