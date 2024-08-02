02e3f8209fb5d07bf31417e5e0f2df1aac6f779f
Allison Blum <118923616+alliblum@users.noreply.github.com>
2024-08-01 21:29:53 -0400

    removed commented out code with a <p> tag that was making next grumpy

--- components/email-form.tsx
+++ components/email-form.tsx
@@ -1,29 +1,5 @@
 import { Box } from '@chakra-ui/react'
 
-// export default function EmailForm() {
-//   return (
-//     <form
-//       action="https://buttondown.email/api/emails/embed-subscribe/alli"
-//       method="post"
-//       target="popupwindow"
-//       onSubmit={() =>
-//         "window.open('https://buttondown.email/alli', 'popupwindow')"
-//       }
-//       className="embeddable-buttondown-form"
-//     >
-//       <label htmlFor="bd-email">Enter your email</label>
-//       <input type="email" name="email" id="bd-email" />
-
-//       <input type="submit" value="Subscribe" />
-//       {/* <p>
-//         <a href="https://buttondown.email/refer/alli" target="_blank">
-//           Powered by Buttondown.
-//         </a>
-//       </p> */}
-//     </form>
-//   )
-// }
-
 export default function EmailForm() {
   return (
     <div>
